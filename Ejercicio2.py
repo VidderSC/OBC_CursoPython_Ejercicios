5de9227739cc6f90f8a9d4ae29e89f7d5961221d
David Som <103112459+VidderSC@users.noreply.github.com>
2023-02-01 10:27:41 +0100

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,3 +1,6 @@
+# Modifica la variable del anterior ejercicio en la consola de python y
+# después muestrala por consola para ver la modificación de la variable.
+
 text="Hola mundo!"
 print(text)
 text="Hola gente!"
