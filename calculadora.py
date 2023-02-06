cd529b4058020554fe2273c3e7cff64e885b852f
David Som <103112459+VidderSC@users.noreply.github.com>
2023-02-06 17:32:35 +0100

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+# Modulo para el ejercicio número 8.
+
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    return a / b
